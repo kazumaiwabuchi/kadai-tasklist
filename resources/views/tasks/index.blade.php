eedfdffd79123544355adfc3fed9bf0a1614f69a
kazuma.iwabuchi <amuzak4342mittuan@gmail.com>
2021-05-12 05:26:30 +0000

    add_signup.get_to_tasks.index_and_navbar

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -3,31 +3,42 @@
 @section('content')
 
 <!-- ここにページ毎のコンテンツを書く -->
-  @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タスク一覧</th>
-                    <th>status</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    {{--タスク詳細ページへのリンク--}}
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->content }}</td>
-                    <td>{{ $task->status }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
+    @if(Auth::check())
+        @if (count($tasks) > 0)
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>タスク一覧</th>
+                        <th>status</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        {{--タスク詳細ページへのリンク--}}
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                        <td>{{ $task->content }}</td>
+                        <td>{{ $task->status }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+        
+        {{-- ページネーションのリンク --}}
+        {{ $tasks->links() }}
+        {{-- タスク作成ページへのリンク --}}
+        {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
+        
+     @else
+        <div class="center jumbotron">
+            <div class="text-center">
+                <h1>Tasklistへようこそ</h1>
+                {{-- ユーザ登録ページへのリンク --}}
+                {!! link_to_route('signup.get', '今すぐ登録する', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+        </div>
     @endif
-    
-    {{-- ページネーションのリンク --}}
-    {{ $tasks->links() }}
-{{-- タスク作成ページへのリンク --}}
-    {!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'btn btn-primary']) !!}
 
 @endsection
